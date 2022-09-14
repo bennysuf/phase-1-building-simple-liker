d83bcea78dc79cf13b921ce7c89283037d817409
Benny Sufrin <bennysuf@gmail.com>
2022-09-14 14:51:38 -0400

    commit tree 6bd77ca35a9c3b91337f86b309045fc179e32b0f

--- main.js
+++ main.js
@@ -3,9 +3,41 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  clickListener()
+})
 
 
 
+function clickListener() { 
+  document.addEventListener('click',(e)=> {
+ if (e.target.classList[0]=== 'like-glyph') { //checks if what i click is the heart
+
+  mimicServerCall()
+  .then((data) => {
+    const activated = e.target.classList.contains('activated-heart'); //to check if it constains the class of activated-heart
+    if (activated) {
+      e.target.classList.remove('activated-heart'); //removes the class that give it red color
+      e.target.innerHTML = EMPTY_HEART
+      
+    } else {
+      e.target.classList.add('activated-heart');
+      e.target.innerHTML = FULL_HEART
+      
+    }
+  })
+  .catch((error) => {
+    return document.querySelector('#modal').classList.remove('hidden'), //removes the hidden class allowing the error to show
+    console.log('Error', error),
+    setTimeout(() => {
+      console.log('return error')
+      return document.getElementById('modal').className = 'hidden'; //adds the hidden class atfer a few seconds to remove error message
+    }, 3000)  
+  })
+ 
+ }
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
